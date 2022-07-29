65caa10798995ecb6a5345baffb1ab35f4006f03
andmatcha <jj.mcr185@gmail.com>
2022-07-29 10:02:18 +0900

    ループ

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,31 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
-
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    let mut count = 0;
+    loop {
+        count += 1;
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please write a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win! ({}times)", count);
+                break;
+            }
+        }
     }
 }
