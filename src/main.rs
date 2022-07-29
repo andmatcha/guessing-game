21d62f969b91c35c4a64aad8b0fc114619a2b93c
andmatcha <jj.mcr185@gmail.com>
2022-07-29 09:43:49 +0900

    入力値と比較

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-
     println!("Please input your guess.");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
     let mut guess = String::new();
 
@@ -11,5 +14,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);     
+    let guess: u32 = guess.trim().parse().expect("Please write a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
